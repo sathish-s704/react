f460672c60388388e3d6527e68e48e16d2a521af
SATHISH S <sathishsathish75363@gmail.com>
2024-12-30 11:22:36 +0530

    count is created

---
+++ src/sample.jsx
@@ -0,0 +1,34 @@
+import { Component } from "react";
+
+class Sample extends Component{
+   
+
+    render() {
+       
+        return (
+           
+        <ol>
+        <li>Fruits</li>
+        <ul>
+            <li>Apple</li>
+            <li>Banana</li>
+            <li>Orange</li>
+        </ul>
+        <li>Vegetables</li>
+        <ul>
+            <li>I. Carrot</li>
+            <li>II. Tomato</li>
+            <li>III. Spinach</li>
+        </ul>
+        <li>Birds</li>
+        <ul class="hash">
+            <li>Crow</li>
+            <li>Sparrow</li>
+            <li>Peacock</li>
+        </ul>
+    </ol>
+
+        );
+    }
+}                   
+export default Sample;
